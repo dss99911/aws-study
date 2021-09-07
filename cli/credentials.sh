d43e7744158e67a4858cf0c2f42bee75cd895639
Hyun <hyun@balancehero.com>
2021-09-08 00:56:34 +0900

    updateupdate

--- cli/credentials.sh
+++ cli/credentials.sh
@@ -1,5 +1,9 @@
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 
+# 1. make user on IAM
+# 2. give permission for the user
+
+
 #Make default credentials.
 # keys, regison, default-format(json)
 aws configure
