5d12c2c568dc6a53a6c1582cbdf47d0ef4078dfd
Hyun <hyun@balancehero.com>
2021-10-31 01:28:20 +0900

    update

--- cli/s3.sh
+++ cli/s3.sh
@@ -2,3 +2,4 @@ aws s3 cp {local path} {s3 path}
 aws s3 cp {s3 path} {s3 path}
 aws s3 cp {s3 path} {local path}
 aws s3 sync {folder_path} s3://{bucket}/{folder_path}
+aws s3 cp s3://Bucket/Folder LocalFolder --recursive
