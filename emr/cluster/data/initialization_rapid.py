3ac08a616d885893d4456d53d760128576ce248b
Hyun <hyun@balancehero.com>
2022-05-13 12:52:19 +0900

    update

--- emr/cluster/data/initialization_rapid.py
+++ emr/cluster/data/initialization_rapid.py
@@ -1,6 +1,8 @@
 from ds_common_utils.pyspark.all import *
 import os
 
+bucket_name = "hyun"
+cluster_name = "test"
 
 def main():
     initialize_spark()
@@ -17,7 +19,7 @@ def initialize_spark():
 
 def configure_livy():
     # livy ignore pyFiles on spark-conf/spark-defaults,
-    os.system("sudo aws s3 cp spark-defaults-livy.conf /etc/livy/conf.dist/spark-defaults.conf")
+    os.system(f"sudo aws s3 cp s3://{bucket_name}/emr/{cluster_name}/spark-defaults-livy.conf /etc/livy/conf.dist/spark-defaults.conf")
 
 
 if __name__ == '__main__':
